d588cfa88fda5be33a387a58cbc421d3c8872706
Kitty Elliott <kitty@glitchcat.space>
2020-03-10 17:24:21 -0700

    majorly updated model

--- src/model.rs
+++ src/model.rs
@@ -1,15 +1,29 @@
 //#![allow(dead_code)]
 
 use rand::Rng;
-use std::collections::BTreeSet as Set;
+use std::collections::HashSet;
 
 pub type MinesweeperModel = Field;
+pub type ModelResult<T> = Result<T, ErrorKind>;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+/**
+ * Enumeration for use in a ModelResult
+ * The `OutOfBounds` variant indicates that the given
+ * coordinates are not in range of the Model's dimensions
+ * The `NoOp` variant indicates that the requested action would
+ * have no change or does not make sense for the given coordinate position
+ */
+pub enum ErrorKind {
+    OutOfBounds,
+    NoOp,
+}
 
 struct Zone {
     flagged: bool,
     revealed: bool,
     has_mine: bool,
-    adj_mine_count: usize,
+    adj_mine_count: u32,
 }
 
 impl Zone {
@@ -21,46 +35,11 @@ impl Zone {
             adj_mine_count: 0,
         }
     }
-
-    pub fn flagged(&self) -> bool {
-        self.flagged
-    }
-
-    pub fn set_flagged(&mut self, value: bool) {
-        self.flagged = value;
-    }
-
-    pub fn revealed(&self) -> bool {
-        self.revealed
-    }
-
-    pub fn reveal(&mut self) {
-        self.revealed = true;
-    }
-
-    pub fn has_mine(&self) -> bool {
-        self.has_mine
-    }
-
-    pub fn adj_mine_count(&self) -> usize {
-        self.adj_mine_count
-    }
-
-    pub fn set_adj_mine_count(&mut self, value: usize) {
-        assert!(value <= 8);
-        self.adj_mine_count = value;
-    }
-
-    pub fn exploded(&self) -> bool {
-        self.has_mine && self.revealed
-    }
 }
 
 pub struct Field {
-    num_mines: usize,
-    num_flagged: usize,
-    num_correctly_flagged: usize,
-    exploded: bool,
+    num_mines: u32,
+    num_flagged: u32,
     grid: Vec<Vec<Zone>>,
 }
 
@@ -68,70 +47,120 @@ impl Field {
     /**
      * Create a new Field. width and height must be greater than 0.
      */
-    pub fn new(width: usize, height: usize, num_mines: usize) -> Self {
-        assert!(width > 0);
-        assert!(height > 0);
-        let placements = Self::generate_placements(num_mines, width, height);
-        let grid = Self::generate_grid(width, height, &placements);
+    pub fn new(width: u32, height: u32, num_mines: u32) -> Option<Self> {
+        if num_mines > width * height {
+            None
+        } else {
+            let mine_placements = Self::generate_placements(num_mines, width, height);
+            Self::with_mine_placements(width, height, mine_placements)
+        }
+    }
+
+    pub fn with_mine_placements(
+        width: u32,
+        height: u32,
+        placements: impl IntoIterator<Item = (u32, u32)>,
+    ) -> Option<Self> {
+        let placements: HashSet<_> = placements.into_iter().collect();
+        if placements.len() > (width * height) as usize {
+            return None;
+        }
         let mut freshly_made = Field {
-            num_mines,
+            num_mines: placements.len() as u32,
             num_flagged: 0,
-            num_correctly_flagged: 0,
-            exploded: false,
-            grid,
+            grid: Self::generate_grid(width, height, &placements),
         };
-        freshly_made.set_counts(&placements);
-        freshly_made
+        freshly_made.set_adj_counts(placements);
+        Some(freshly_made)
     }
 
     /**
      * The height of this Field
      */
-    pub fn height(&self) -> usize {
-        self.grid.len()
+    pub fn height(&self) -> u32 {
+        self.grid[0].len() as u32
     }
 
     /**
      * The width of this Field
      */
-    pub fn width(&self) -> usize {
-        self.grid[0].len()
+    pub fn width(&self) -> u32 {
+        self.grid.len() as u32
     }
 
     /**
      * The number of mines buried in this Field
      */
-    pub fn num_mines(&self) -> usize {
+    pub fn num_mines(&self) -> u32 {
         self.num_mines
     }
 
     /**
-     * The total number of flags planted in Zones on this Field
+     * The total number of flags planted on this Field
      */
-    pub fn num_flagged(&self) -> usize {
+    pub fn num_flagged(&self) -> u32 {
         self.num_flagged
     }
 
     /**
-     * The number of flags placed on this Zones in this Field
-     * in which mines are buried
+     * boolean indicating if there is a flag planted at the given coordinates
      */
-    pub fn num_correctly_flagged(&self) -> usize {
-        self.num_correctly_flagged
+    pub fn is_flagged_at(&self, x: u32, y: u32) -> Option<bool> {
+        self.zone_at(x, y).map(|z| z.flagged)
     }
 
     /**
-     * True if the game has been lost
+     * if `add` is true, adds a flag, otherwise removes a flag
+     * if trying to add a flag to a zone that is already flagged,
+     * or trying to remove a flag from a zone without a flag, then
+     * nothing will be done and Err(ErrorKind::NoOp) will be returned.
      */
-    pub fn exploded(&self) -> bool {
-        self.exploded
+    pub fn change_flag_at(&mut self, x: u32, y: u32, new_flag_value: bool) -> ModelResult<()> {
+        let zone = self.zone_at_mut(x, y).ok_or(ErrorKind::OutOfBounds)?;
+        if zone.flagged == new_flag_value {
+            return Err(ErrorKind::NoOp);
+        }
+        zone.flagged = new_flag_value;
+        if new_flag_value {
+            self.num_flagged += 1;
+        } else {
+            self.num_flagged -= 1;
+        }
+        Ok(())
+    }
+
+    pub fn is_revealed_at(&self, x: u32, y: u32) -> Option<bool> {
+        self.zone_at(x, y).map(|z| z.revealed)
     }
 
     /**
-     * TODO
+     * returns:
+     *  on success, returns a boolean indicating if the revealed zone
+     *     contains a mine
+     *  ErrorKind::NoOp indicates that the zone at the given
+     *      coordinates has already been revealed
      */
-    pub fn explode(&mut self) {
-        self.exploded = true;
+    pub fn reveal_at(&mut self, x: u32, y: u32) -> ModelResult<bool> {
+        let zone = self.zone_at_mut(x, y).ok_or(ErrorKind::OutOfBounds)?;
+        if zone.revealed {
+            Err(ErrorKind::NoOp)
+        } else {
+            zone.revealed = true;
+            Ok(zone.has_mine)
+        }
+    }
+
+    pub fn has_mine_at(&self, x: u32, y: u32) -> Option<bool> {
+        self.zone_at(x, y).map(|z| z.has_mine)
+    }
+
+    /**
+     * if the given coordinates are within the dimensions of the Field,
+     * returns the number of positions adjacent to the given position
+     * which contain a buried mine
+     */
+    pub fn mines_adjacent_to(&self, x: u32, y: u32) -> Option<u32> {
+        self.zone_at(x, y).map(|z| z.adj_mine_count)
     }
 
     /**
@@ -140,12 +169,9 @@ impl Field {
      * If include_diag is true, then diagonal adjacencies will be included.
      * TODO: expound
      */
-    pub fn adjacent_positions(
-        &self,
-        x: usize,
-        y: usize,
-        include_diag: bool,
-    ) -> Vec<(usize, usize)> {
+    pub fn adjacent_positions(&self, x: u32, y: u32, include_diag: bool) -> Vec<(u32, u32)> {
+        let x = x as i32;
+        let y = y as i32;
         let mut positions;
         if include_diag {
             // there are at most 8 adjacent positions
@@ -162,93 +188,30 @@ impl Field {
         }
         positions
             .into_iter()
+            // safely convert back to u32
+            .filter(|&(x, y)| x >= 0 && y >= 0)
+            .map(|(x, y)| (x as u32, y as u32))
             // filter out coordinates that aren't in-bounds
-            .filter(|(x, y)| self.get_zone_at(*x, *y).is_some())
+            .filter(|&(x, y)| self.zone_at(x, y).is_some())
             .collect()
     }
 
     /**
-     * Flag the zone at the given coordinates. If the zone at those
-     * coordinates is already flagged, nothing happens.
-     * Returns Err if given coordinates were out of bounds, else Ok
-     */
-    pub fn flag_zone(&mut self, x: usize, y: usize) -> Result<(), ()> {
-        let zone = match self.get_zone_at_mut(x, y) {
-            Some(z) => z,
-            None => return Err(()),
-        };
-        if !zone.flagged() {
-            zone.set_flagged(true);
-            if zone.has_mine() {
-                self.num_correctly_flagged += 1;
-            }
-            self.num_flagged += 1;
-        }
-        Ok(())
-    }
-
-    /**
-     * Unflag the zone at the given coordinates. If the zone at those
-     * coordinates is already not flagged, nothing happens.
-     * Returns Err if given coordinates were out of bounds, else Ok
-     */
-    pub fn unflag_zone(&mut self, x: usize, y: usize) -> Result<(), ()> {
-        let zone = match self.get_zone_at_mut(x, y) {
-            Some(z) => z,
-            None => return Err(()),
-        };
-        if zone.flagged() {
-            zone.set_flagged(false);
-            if zone.has_mine() {
-                self.num_correctly_flagged -= 1;
-            }
-            self.num_flagged -= 1;
-        }
-        Ok(())
-    }
-
-    /**
-     * TODO!
-     */
-    pub fn reveal_zone(&mut self, x: usize, y: usize) -> Result<(), ()> {
-        let zone = match self.get_zone_at_mut(x, y) {
-            Some(z) => z,
-            None => return Err(()),
-        };
-        if !zone.revealed() {
-            zone.reveal();
-            if zone.has_mine() {
-                self.explode();
-            } else {
-                if zone.adj_mine_count() == 0 {
-                    for (adj_x, adj_y) in self.adjacent_positions(x, y, false) {
-                        let adj_zone = &mut self.grid[adj_x][adj_y];
-                        if !adj_zone.has_mine() && adj_zone.adj_mine_count() == 0 {
-                            self.reveal_zone(adj_x, adj_y).unwrap();
-                        }
-                    }
-                }
-            }
-        }
-        Ok(())
-    }
-
-    /**
      * Updates the adjacent mine counts for Zones in this Field.
-     * Shouldn't need to be called more than once.
+     * Shouldn't be called more than once.
      */
-    fn set_counts(&mut self, mine_placements: &Set<(usize, usize)>) {
+    fn set_adj_counts(&mut self, mine_placements: HashSet<(u32, u32)>) {
         // reset all counts to 0.
-        for x in 0..self.width() {
-            for y in 0..self.height() {
-                self.grid[x][y].set_adj_mine_count(0);
-            }
-        }
+        // for x in 0..self.width() {
+        //     for y in 0..self.height() {
+        //         self.grid[x][y].adj_mine_count = 0;
+        //     }
+        // }
         for (x, y) in mine_placements {
-            for (adj_x, adj_y) in self.adjacent_positions(*x, *y, true) {
-                let zone = &mut self.grid[adj_x][adj_y];
+            for (adj_x, adj_y) in self.adjacent_positions(x, y, true) {
+                let zone = &mut self.grid[adj_x as usize][adj_y as usize];
                 // increment mine count
-                zone.set_adj_mine_count(zone.adj_mine_count() + 1);
+                zone.adj_mine_count += 1;
             }
         }
     }
@@ -256,27 +219,28 @@ impl Field {
     /**
      * TODO
      */
-    fn get_zone_at(&self, x: usize, y: usize) -> Option<&Zone> {
-        self.grid.get(x)?.get(y)
+    fn zone_at(&self, x: u32, y: u32) -> Option<&Zone> {
+        self.grid.get(x as usize)?.get(y as usize)
     }
 
     /**
      * TODO
      */
-    fn get_zone_at_mut(&mut self, x: usize, y: usize) -> Option<&mut Zone> {
-        self.grid.get_mut(x)?.get_mut(y)
+    fn zone_at_mut(&mut self, x: u32, y: u32) -> Option<&mut Zone> {
+        self.grid.get_mut(x as usize)?.get_mut(y as usize)
     }
 
     /**
      * TODO
      */
     fn generate_placements(
-        num_mines: usize,
-        upper_x_bound: usize,
-        upper_y_bound: usize,
-    ) -> Set<(usize, usize)> {
+        num_mines: u32,
+        upper_x_bound: u32,
+        upper_y_bound: u32,
+    ) -> HashSet<(u32, u32)> {
+        let num_mines = num_mines as usize;
         let mut rng = rand::thread_rng();
-        let mut coordinates = Set::new();
+        let mut coordinates = HashSet::with_capacity(num_mines);
         while coordinates.len() < num_mines {
             let x = rng.gen_range(0, upper_x_bound);
             let y = rng.gen_range(0, upper_y_bound);
@@ -289,18 +253,19 @@ impl Field {
      * TODO
      */
     fn generate_grid(
-        width: usize,
-        height: usize,
-        mine_placements: &Set<(usize, usize)>,
+        width: u32,
+        height: u32,
+        mine_placements: &HashSet<(u32, u32)>,
     ) -> Vec<Vec<Zone>> {
-        let mut columns = Vec::with_capacity(width);
+        let mut grid = Vec::with_capacity(width as usize);
         for x in 0..width {
-            let mut col = Vec::with_capacity(height);
+            let mut column = Vec::with_capacity(height as usize);
             for y in 0..height {
-                col.push(Zone::new(mine_placements.contains(&(x, y))));
+                let has_mine = mine_placements.contains(&(x, y));
+                column.push(Zone::new(has_mine));
             }
-            columns.push(col);
+            grid.push(column);
         }
-        columns
+        grid
     }
 }
